722edb581c4c1982470a502b54fd2269ffb72ca3
drewablank <drewablank@outlook.com>
2018-11-26 11:36:38 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,7 +29,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include profile::base
+  # include profile::base
 }
 
 node lb.puppet.vlan {
